1f38b9fcc451b667fc320d5f47367d20398576ec
hardik dave <ce.hardikdave@gmail.com>
2026-01-30 17:40:39 +0530

    Added extent report and running via mastertestsuite to generate the html report

--- src/test/java/testBase/baseClass.java
+++ src/test/java/testBase/baseClass.java
@@ -1,11 +1,16 @@
 package testBase;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -60,5 +65,21 @@ public class baseClass
 		driver.quit();
 	}
 	
+	
+	public String captureScreen(String tname)
+	{
+		System.out.println("From base class");
+		String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		
+		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+		File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+		
+		String targetFilePath=System.getProperty("user.dir")+"\\screenshots\\" + tname + "_" + timeStamp + ".png";
+		File targetFile=new File(targetFilePath);
+		
+		sourceFile.renameTo(targetFile);
+			
+		return targetFilePath;
+	}
 	
 }
